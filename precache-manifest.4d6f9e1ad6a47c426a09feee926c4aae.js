03fdf4036374ae2c321237c6be21c8c10573e2d2
zhu-tom <zhutom01@gmail.com>
2020-06-09 02:08:05 -0400

    Updates

---
+++ precache-manifest.4d6f9e1ad6a47c426a09feee926c4aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47086ccb8940ab1aec05f935d8c2dc3b",
+    "url": "/Game-of-Life/index.html"
+  },
+  {
+    "revision": "c2518ae02a2ba267f7e5",
+    "url": "/Game-of-Life/static/css/main.273124ad.chunk.css"
+  },
+  {
+    "revision": "10502fad10478d146c96",
+    "url": "/Game-of-Life/static/js/2.3b4e85b0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Game-of-Life/static/js/2.3b4e85b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2518ae02a2ba267f7e5",
+    "url": "/Game-of-Life/static/js/main.9b8749c6.chunk.js"
+  },
+  {
+    "revision": "962914dffbc5fbf9472d",
+    "url": "/Game-of-Life/static/js/runtime-main.c58097f8.js"
+  }
+]);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './components/game';
-import './styles.css';
-
-ReactDOM.render(<Game/>, document.getElementById('root'));
